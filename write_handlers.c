614db8ffe4fa7b50aa36fad7147c3b7e715ec595
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-08-11 14:16:11 -0700

    commit

--- write_handlers.c
+++ write_handlers.c
@@ -71,7 +71,7 @@ if (is_negative)
 extra_ch = '-';
 else if (flags & F_PLUS)
 extra_ch = '+';
-else if (flags & F_SPACE)
+else (flags & F_SPACE)
 extra_ch = ' ';
 
 return (write_num(ind, buffer, flags, width, precision,
